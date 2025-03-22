8d0601163bbc8aebf3f2c4da4024a01eb9cbd546
Nahia Otegui <nay.fullstack@gmail.com>
2025-03-22 13:46:01 +0100

    primer commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let variableSinValor
+let booleano1 = true;
+let booleano2 = false;
+const PI = 3.14;
+const TAU = PI * 2;
+let booleanoAnd = booleano1 && booleano2;
+let booleanoNot = !booleano1;
+let booleanoMix0 = (booleano1 || booleano2) && (booleano1 || ( !booleano1 && !booleano2));
+let contarHasta10_2 = 0;
+for(contarHasta10_2 ===10){
+
+}
